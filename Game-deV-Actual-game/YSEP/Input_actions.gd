9b2ab52bb56260cdce7527052a6c937496d1a818
UnbearableFox <deadgamer07yt@gmail.com>
2024-05-15 20:07:36 -0400

    Add files via upload

--- Game-deV-Actual-game/YSEP/Input_actions.gd
+++ Game-deV-Actual-game/YSEP/Input_actions.gd
@@ -14,14 +14,16 @@ func _ready(): # one time at start
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	velocity = velocity.normalized()
 
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
 	if Input.is_action_pressed("shoot"):
 		shoot()
 	# permet de d'avoir la même vitesse en diagonal
-	velocity = velocity.normalized()
+	
 	
 	set_velocity(velocity*speed)
 	move_and_slide()
--- Game-deV-Actual-game/YSEP/Projectile.gd
+++ Game-deV-Actual-game/YSEP/Projectile.gd
@@ -1,19 +1,30 @@
-extends Area2D
+extends Node2D
 
 @export var speed :int = 10 #speed of the bullet
 var Scene_Principal = preload("res://node_2d.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	set_process(true) # Replace with function body.
+var temp_sprite
+var timer = Timer
 
+
+
+func _ready(): # Called when the node enters the scene tree for the first time.
+	set_process(true) # Replace with function body.
+	var timer = get_node("Timer")
+	
+	
+func _timer():
+	timer.timeout.connect()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction = $PlayerTopDown/Direction.target_position #direction of bullet
-	position += direction * speed * delta
+	position += transform.x * speed * delta
 	if is_out_of_screen():
 		queue_free()
-		
+
 func is_out_of_screen():
 	var viewport_rect = get_viewport_rect()
 	return !viewport_rect.has_point(position)
+
+func _on_Timer_timeout():
+	queue_free()  # Delete the object
+
